994fbbb76bfceee62b59dd061635c58a709f83d2
Mark(Git) <marky.mikhail8@gmail.com>
2023-10-10 20:21:15 -0400

    fix updating roles bug finally

--- main.py
+++ main.py
@@ -892,7 +892,8 @@ async def updateRoles(ctx: interactions.CommandContext):
         embed = await successEmbed("No new roles added.")
     else:
         embed = await successEmbed("Added role(s): " + rolesList)
-    await d.edit(embeds=embed, content="")
+    await d.delete()
+    await ctx.send(embeds=embed, content="")
 
 
 @bot.command(name="linkdiscord", description="Link a user's discord account to their Challenge List account",
